b1f42360b8c2f01b3fe0b83a086823f19479d6b0
Tamer AbdulRadi <tamer@abdulradi.com>
2016-12-30 23:36:25 +0000

    Test cases for collection codec fetching

--- troy-driver/src/test/scala/troy/driver/schema/CodecForColumnSpec.scala
+++ troy-driver/src/test/scala/troy/driver/schema/CodecForColumnSpec.scala
@@ -9,14 +9,18 @@ import troy.driver.codecs.PrimitivesCodecs.intAsInt
 class CodecForColumnSpec extends FlatSpec with Matchers {
   implicit val column1 = ColumnType.instance[1, "my_keyspace", "my_table", "my_column_1", CDT.Ascii]
   implicit val column2 = ColumnType.instance[1, "my_keyspace", "my_table", "my_column_2", CDT.Int]
+  implicit val column3 = ColumnType.instance[1, "my_keyspace", "my_table", "my_column_3", CDT.List[CDT.Int]]
+  implicit val column4 = ColumnType.instance[1, "my_keyspace", "my_table", "my_column_4", CDT.Map[CDT.Int, CDT.Ascii]]
 
   CodecForColumn[1, "my_keyspace", "my_table", "my_column_1"].codec: TroyCodec[CDT.Ascii, String]
   CodecForColumn[1, "my_keyspace", "my_table", "my_column_2"].codec: TroyCodec[CDT.Int, Int]
+  CodecForColumn[1, "my_keyspace", "my_table", "my_column_3"].codec: TroyCodec[CDT.List[CDT.Int], Seq[Int]]
+  CodecForColumn[1, "my_keyspace", "my_table", "my_column_4"].codec: TroyCodec[CDT.Map[CDT.Int, CDT.Ascii], Map[Int, String]]
 
   "Fetching codec for non existing column" should "fail" in {
     illTyped(
       """
-        CodecForColumn[1, "my_keyspace", "my_table", "my_column_3"].codec
+        CodecForColumn[1, "my_keyspace", "my_table", "my_column_99"].codec
       """)
   }
 }
