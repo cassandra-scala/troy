fa4350a5972bea88c23df1a5955dfe2569ae69ef
Tamer AbdulRadi <tamer@abdulradi.com>
2017-01-08 22:45:27 +0000

    fixup! High level internal-dsl to be generated by macro

--- troy-driver/src/test/scala/troy/driver/driver/SelectSpec.scala
+++ troy-driver/src/test/scala/troy/driver/driver/SelectSpec.scala
@@ -1,57 +1,53 @@
 package troy
 package driver.driver
 
-import org.scalatest.{FlatSpec, Matchers}
 import shapeless._
-import shapeless.test.illTyped
-import troy.driver.codecs.TroyCodec
-import troy.driver.query.select.Select
-import troy.driver.schema.column.{CodecForColumn, ColumnType}
+import troy.driver.query.select.Selection
 import troy.driver.{CassandraDataType => CDT}
 import troy.driver.query.select._
 
-object SelectSpec {
+object SelectionSpec {
   import Select._
   import TestSchema._
 
-  def assertSelectOut[V, K, T, S, O](implicit s: Select.Aux[V, K, T, S, O]) = ()
+  def assertSelectionOut[V, K, T, S, O](implicit s: Selection.Aux[V, K, T, S, O]) = ()
 
-  assertSelectOut[1, "test", "posts",
+  assertSelectionOut[1, "test", "posts",
     HNil,
     HNil
   ]
 
   // Columns
-  assertSelectOut[1, "test", "posts",
+  assertSelectionOut[1, "test", "posts",
     Column["author_id"] :: HNil,
     CDT.Uuid :: HNil
   ]
 
-  assertSelectOut[1, "test", "posts",
+  assertSelectionOut[1, "test", "posts",
     Column["author_id"] :: Column["post_id"] :: HNil,
     CDT.Uuid :: CDT.TimeUuid :: HNil
   ]
 
   // Functions only
-  assertSelectOut[1, "test", "posts",
+  assertSelectionOut[1, "test", "posts",
     Function[NoKeyspace, "now", HNil] :: HNil,
     CDT.TimeUuid :: HNil
   ]
 
   // Functions on columns
-  assertSelectOut[1, "test", "posts",
+  assertSelectionOut[1, "test", "posts",
     Function[NoKeyspace, "writetime", Column["author_id"] :: HNil] :: HNil,
     CDT.BigInt :: HNil
   ]
 
   // Functions AND columns
-  assertSelectOut[1, "test", "posts",
+  assertSelectionOut[1, "test", "posts",
     Column["author_id"] :: Function[NoKeyspace, "writetime", Column["author_id"] :: HNil] :: HNil,
     CDT.Uuid :: CDT.BigInt :: HNil
   ]
 
   // Functions on Functions
-  assertSelectOut[1, "test", "posts",
+  assertSelectionOut[1, "test", "posts",
     Function[NoKeyspace, "dateof", Function[NoKeyspace, "now", HNil] :: HNil] :: HNil,
     CDT.Timestamp :: HNil
   ]
