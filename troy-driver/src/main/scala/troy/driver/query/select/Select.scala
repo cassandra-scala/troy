6602c0309596c9e264dc182e802dd57ae2fbc36d
Tamer AbdulRadi <tamer@abdulradi.com>
2017-01-08 16:43:36 +0000

    High level internal-dsl to be generated by macro

--- troy-driver/src/main/scala/troy/driver/query/select/Select.scala
+++ troy-driver/src/main/scala/troy/driver/query/select/Select.scala
@@ -1,19 +1,26 @@
 package troy.driver.query.select
 
 import shapeless._
-import troy.driver.schema.keyspace.KeyspaceExists
-import troy.driver.schema.table.TableExists
-import troy.driver.schema.version.VersionExists
+import troy.driver.schema.{ FunctionType, KeyspaceExists, TableExists, VersionExists }
 import troy.driver.schema.column.ColumnType
-import troy.driver.schema.FunctionType
 
-trait Select[Version, Keyspace, Table, Selection] {
-  type Out <: HList
+import scala.concurrent.Future
+
+trait Select[I, O] {
+  type RawType <: HList
+  type ParamsType <: HList
+
+  def rawQuery: String
+
+  def apply(i: I): O = {
+    println(rawQuery)
+    ???
+  }
 }
 
 object Select {
-  type Aux[V, K, T, S, O] = Select[V, K, T, S] { type Out = O }
-  def apply[V, K, T, S](implicit s: Select[V, K, T, S]): Aux[V, K, T, S, s.Out] = s
+  //  type Aux[V, K, T, S, O] = Select[V, K, T, S] { type Out = O }
+  //  def apply[V, K, T, S](implicit s: Select[V, K, T, S]): Aux[V, K, T, S, s.Out] = s
 
   // TODO
   // trait Aliased[S, Alias]
@@ -22,26 +29,15 @@ object Select {
   // trait Asterisk
   trait NoKeyspace // Used to mark absence of a specific keyspace
 
-  implicit def hNilInstance[V, K, T](
+  def select[V, K, T, S, I, O](raw: String)(
     implicit
-    tableExists: TableExists[V, K, T]
-  ): Aux[V, K, T, HNil, HNil] =
-    new Select[V, K, T, HNil] { type Out = HNil }
-
-  implicit def hColumnInstance[V, K, T, ColumnName, ST <: HList](
-    implicit
+    versionExists: VersionExists[V],
+    keyspaceExists: KeyspaceExists[V, K],
     tableExists: TableExists[V, K, T],
-    headType: ColumnType[V, K, T, ColumnName],
-    tailType: Select[V, K, T, ST]
-  ): Aux[V, K, T, Column[ColumnName] :: ST, headType.Out :: tailType.Out] =
-    new Select[V, K, T, Column[ColumnName] :: ST] { type Out = headType.Out :: tailType.Out }
-
-  implicit def hFunctionInstance[V, K, T, FKeyspace, FName, FParams <: HList, FParamTypes <: HList, ST <: HList](
-    implicit
-    tableExists: TableExists[V, K, T],
-    fParamTypes: Select.Aux[V, K, T, FParams, FParamTypes], // FIXME: I don't like resuing the same typeclass to query param types, maybe rename the whole Select typeclass?
-    headType: FunctionType[V, FKeyspace, FName, FParamTypes],
-    tailType: Select[V, K, T, ST]
-  ): Aux[V, K, T, Function[FKeyspace, FName, FParams] :: ST, headType.Out :: tailType.Out] =
-    new Select[V, K, T, Function[FKeyspace, FName, FParams] :: ST] { type Out = headType.Out :: tailType.Out }
+    selection: Selection[V, K, T, S]
+  ) = new Select[I, Future[O]] {
+    override type RawType = selection.Out
+    override type ParamsType = Nothing
+    override val rawQuery = raw
+  }
 }
