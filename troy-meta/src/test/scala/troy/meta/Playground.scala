02df534b64852eb204fa1d114c609b25352f5f93
Tamer AbdulRadi <tamer@abdulradi.com>
2017-01-08 16:45:20 +0000

    Simple test for new macros

---
+++ troy-meta/src/test/scala/troy/meta/Playground.scala
@@ -0,0 +1,18 @@
+package troy.meta
+
+import java.util.UUID
+
+import com.datastax.driver.core.utils.UUIDs
+
+import scala.concurrent.Future
+
+@schema object Schema
+
+object Playground extends App {
+  def query[I, O](x: String): I => Future[O] = ???
+  case class Post(id: UUID, title: String)
+
+  import Schema._
+  @schemasafe val getAuthorPosts = query[(UUID, Int), Post]("select post_id, post_title from test.posts where authorId = ? AND post_rating >= ?;")
+  getAuthorPosts((UUIDs.timeBased(), 4)): Future[Post]
+}
